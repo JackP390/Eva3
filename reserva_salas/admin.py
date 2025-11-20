50e454cf0834157c69bae865df49ef46665dfcf0
JackP390 <patatamati123@gmail.com>
2025-11-20 01:58:19 -0300

    creación de validaciones

--- reserva_salas/admin.py
+++ reserva_salas/admin.py
@@ -2,6 +2,18 @@ from django.contrib import admin
 from .models import SalaEstudio
 from .models import Reserva
 
+class SalaAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'capacidad', 'disponible', 'en_mantenimiento')
+    list_filter = ('disponible', 'en_mantenimiento')
+    search_fields = ('nombre',)
+    list_editable = ('disponible', 'en_mantenimiento')
+    list_per_page = 10
+
+class ReservaAdmin(admin.ModelAdmin):
+    list_display = ('sala_reservada', 'rut_reservante', 'hora_inicio', 'hora_termino')
+    list_filter = ('sala_reservada', 'hora_inicio')
+    search_fields = ('rut_reservante',)
+
 # Register your models here.
 admin.site.register(SalaEstudio)
 admin.site.register(Reserva)
--- reserva_salas/forms.py
+++ reserva_salas/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from .models import Reserva
 
 class ReservaForm(forms.ModelForm):
@@ -7,3 +8,20 @@ class ReservaForm(forms.ModelForm):
         fields = ['rut_reservante']
         widgets = {'rut_reservante':forms.TextInput(attrs={'class':'form-control', 'placeholder':'12345678-9'})}
         labels = {'rut_reservante':'Ingresa tu RUT para confirmar'}
+
+    def clean_rut_reservante(self):
+        rut = self.cleaned_data.get('rut_reservante')
+
+        rut = rut.replace('.', '')
+
+        if '-' not in rut:
+            raise ValidationError("El RUT debe incluir el guion (Ej: 11111111-1)")
+        
+        if len(rut) < 8:
+            raise ValidationError("El RUT parece demasiado corto.")
+        
+        cuerpo, dv = rut.split('-')
+        if len(dv) != 1:
+            raise ValidationError("El dígito verificador debe ser un solo caracter.")
+        
+        return rut
