5a72b6a3961f365cc8cdb237428ce3ea1ab03c69
JackP390 <patatamati123@gmail.com>
2025-11-19 23:44:15 -0300

    Se crea el archivo form junto al formulario de reserva

---
+++ reserva_salas/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import Reserva
+
+class ReservaForm(forms.ModelForm):
+    class Meta:
+        model = Reserva
+        fields = ['rut_reservante']
+        widgets = {'rut_reservante':forms.TextInput(attrs={'class':'form-control', 'placeholder':'12345678-9'})},
+        labels = {'rut_reservante':'Ingresa tu RUT para confirmar'},
